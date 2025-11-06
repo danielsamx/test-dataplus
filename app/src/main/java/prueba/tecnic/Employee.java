b3aee8b8b7d9c63007234678ba4aa16e88261b32
danielsamx <192350178+danielsamx@users.noreply.github.com>
2025-11-06 12:34:15 -0500

    docs: Document the class: getters and functions

--- app/src/main/java/prueba/tecnic/Employee.java
+++ app/src/main/java/prueba/tecnic/Employee.java
@@ -1,27 +1,48 @@
 package main.java.prueba.tecnic;
 
+/**
+ * This class employee has information about your id, name, lastname and base salary.
+ * id is private because only can access for people with major access
+ * name and lastname are public because need see information
+ * baseSalary is protected because is confidentiality
+ */
 public class Employee {
 
     private int id;
-    public string name;
-    public string lastname;
+    public String name;
+    public String lastname;
     protected float baseSalary;
 
-    public Employee(int id, string name, string lastname, float baseSalary) {
+    /**
+     * Initiliaze the class
+     */
+    public Employee(int id, String name, String lastname, float baseSalary) {
         this.id = id;
         this.name = name;
         this.lastname = lastname;
         this.baseSalary = baseSalary;
     }
 
-    public string getName() {
+    /**
+     * Get name of employee
+     * @return name
+     */
+    public String getName() {
         return name;
     }
 
-    public string getLastname() {
+    /**
+     * Get lastname of employee
+     * @return lastname
+     */
+    public String getLastname() {
         return lastname;
     }
 
+    /**
+     * Get base salary of employee
+     * @return baseSalary
+     */
     public float getBaseSalary() {
         return baseSalary;
     }
@@ -30,11 +51,11 @@ public class Employee {
         this.id = id;
     }
 
-    public void setName(string name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setLastname(string lastname) {
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
@@ -42,11 +63,19 @@ public class Employee {
         this.baseSalary = baseSalary;
     }
 
+    /**
+     * Calculta salary for each employee
+     * @return baseSalary
+     */
     @Override
     public float calculateSalary(){
-        return 0;
+        return baseSalary;
     }
 
+    /**
+     * Show employee information
+     * @return all data for employee
+     */
     @Override
     public String showInformation() {
         return "Employee [id=" + id + ", name=" + name + ", lastname=" + lastname + ", baseSalary=" + baseSalary + "]";
