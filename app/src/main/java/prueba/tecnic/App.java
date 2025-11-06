04407c7d729fd7e864f1fb749c69b5910f8941dd
danielsamx <192350178+danielsamx@users.noreply.github.com>
2025-11-06 12:53:56 -0500

    feat: Create objects for each employee type and show information with your salary

--- app/src/main/java/prueba/tecnic/App.java
+++ app/src/main/java/prueba/tecnic/App.java
@@ -3,29 +3,22 @@ package prueba.tecnic;
 import main.java.prueba.tecnic.Employee;
 import main.java.prueba.tecnic.FullTimeEmployee;
 import main.java.prueba.tecnic.HourPayEmployee;
-/**
- * Principal class for create many instances for employees
- */
-public class App {
 
+public class App {
     public static void main(String[] args) {
 
-        // Class Initialize
+        // Initialize employees
         Employee employee = new Employee(1, "Anderson", "Cango", 450.00);
-        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(2, "Daniel", "Samuisa", 450.00);
-        HourPayEmployee hourPayEmployee = new HourPayEmployee(3, "Juan", "Salinas", 450.00);
+        Employee fullTimeEmployee = new FullTimeEmployee(2, "Daniel", "Samuisa", 450.00);
+        Employee hourPayEmployee = new HourPayEmployee(3, "Juan", "Salinas", 8, 5.50 );
 
-        // Show information for employee
-        System.out.println("Employee Information:");
-        System.out.println(employee.showInformation());
-        System.out.println(employee.calculateSalary());
-
-        System.out.println("Full Time Employee Information:");
-        System.out.println(fullTimeEmployee.showInformation());
-        System.out.println(fullTimeEmployee.calculateSalary());
-
-        System.out.println("Hour Pay Employee Information:");
-        System.out.println(hourPayEmployee.showInformation());
-        System.out.println(hourPayEmployee.calculateSalary(8, 5.50));
+        // Poliform action with each employee type
+        // Show information and depends salary for each type
+        System.out.println("Employee salary: " + employee.calculateSalary());
+        System.out.println("Employee salary: " + employee.showInformation());
+        System.out.println("Full-time salary: " + fullTimeEmployee.calculateSalary());
+        System.out.println("Full-time salary: " + fullTimeEmployee.showInformation());
+        System.out.println("Hourly salary: " + hourPayEmployee.calculateSalary());
+        System.out.println("Hourly salary: " + hourPayEmployee.showInformation());
     }
 }
