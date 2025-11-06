512ef95f96d064b7fe7cd83047e1f11d723f3690
danielsamx <192350178+danielsamx@users.noreply.github.com>
2025-11-06 12:36:36 -0500

    docs: Document the class: override function

--- app/src/main/java/prueba/tecnic/HourPayEmployee.java
+++ app/src/main/java/prueba/tecnic/HourPayEmployee.java
@@ -7,6 +7,14 @@ package main.java.prueba.tecnic;
  */
 public class HourPayEmployee extends Employee{
 
+    public HourPayEmployee(){
+        super();
+    }
+
+    /**
+     * Calculate salary
+     * In this case hours per price for this hours
+     */
     @Override
     public float calculateSalary(int hours, float price){
         return hours * price;
