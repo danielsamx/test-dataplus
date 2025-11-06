e7133f612df3b8d9ab41fe538dab454ef5b01fa0
danielsamx <192350178+danielsamx@users.noreply.github.com>
2025-11-06 12:35:48 -0500

    docs: Document the class: override function

--- app/src/main/java/prueba/tecnic/FullTimeEmployee.java
+++ app/src/main/java/prueba/tecnic/FullTimeEmployee.java
@@ -2,6 +2,17 @@ package main.java.prueba.tecnic;
 
 public class FullTimeEmployee extends Employee{
 
+    /**
+     * Apply data with employee with super
+     */
+    public FullTimeEmployee() {
+        super();
+    }
+
+    /**
+     * Calculate salary
+     * In this case the base salary plus 10 percent of base salary
+     */
     @Override
     public float calculateSalary(){
         return baseSalary + (baseSalary*0.1);
