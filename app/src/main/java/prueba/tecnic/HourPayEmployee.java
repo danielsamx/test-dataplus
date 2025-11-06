7d31f262fa35f9aa955abae0063b876cfdddeda2
danielsamx <192350178+danielsamx@users.noreply.github.com>
2025-11-06 12:36:47 -0500

    docs: Document the class: override function

--- app/src/main/java/prueba/tecnic/HourPayEmployee.java
+++ app/src/main/java/prueba/tecnic/HourPayEmployee.java
@@ -1,10 +1,5 @@
 package main.java.prueba.tecnic;
 
-
-/**
- *  EmpleadoPorHoras: su salario se calcula como el número de horas trabajadas * pago por hora 
- * (debes pedir o definir estos valores).
- */
 public class HourPayEmployee extends Employee{
 
     public HourPayEmployee(){
